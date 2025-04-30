0e4fd5f8cf3fa82b912e38f84cf812acad64062a
Pepe de la Madrid <PPDLM>
2025-04-30 17:17:31 -0600

    CONFIGURATION

--- src/camera_pkg/setup.py
+++ src/camera_pkg/setup.py
@@ -4,25 +4,28 @@ package_name = 'camera_pkg'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.1.0',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
-    install_requires=['setuptools'],
+    install_requires=[
+        'setuptools',
+        'opencv-python',
+        'ultralytics',
+        'numpy'
+    ],
     zip_safe=True,
     maintainer='robotec',
     maintainer_email='a01640939@tec.mx',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    description='Detección de rocas con YOLOv8 en ROS 2 Humble',
+    license='Apache-2.0',
     tests_require=['pytest'],
     entry_points={
-    'console_scripts': [
-        'rock_detector_node = camera_pkg.framepublisher:main',
-        'pixel_to_point3d = camera_pkg.pixel_to_point3d:main',
+        'console_scripts': [
+            'rock_detector = camera_pkg.rock_detector_node:main',
         ],
     },
-
 )
