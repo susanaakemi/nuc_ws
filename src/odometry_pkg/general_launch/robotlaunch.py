48354aedea125ae24692d34a169e35ac8a78bd90
SaJaMa06 <sajama2006@gmail.com>
2025-05-06 23:46:50 -0600

    Update robotlaunch.py

--- src/odometry_pkg/general_launch/robotlaunch.py
+++ src/odometry_pkg/general_launch/robotlaunch.py
@@ -1,1 +1,41 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
 
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='arm_pkg',
+            executable='clicked_point_subscriber',
+            name='clicked_point_subscriber'
+        ),
+        Node(
+            package='camera_pkg',
+            executable='framepublisher',
+            name='framepublisher'
+        ),
+        Node(
+            package='camera_pkg',
+            executable='pixel_to_point3d',
+            name='pixel_to_point3d'
+        ),
+        Node(
+            package='odometry_pkg',
+            executable='controller',
+            name='controller'
+        ),
+        Node(
+            package='odometry_pkg',
+            executable='efk',
+            name='ExtendedKalmanFilter'
+        ),
+        Node(
+            package='odometry_pkg',
+            executable='odometry_node',
+            name='odometry'
+        ),
+        Node(
+            package='odometry_pkg',
+            executable='utils',
+            name='utils'
+        ),
+    ])
